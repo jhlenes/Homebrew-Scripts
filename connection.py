4854f9671d19cdd9aa7ef8c4e9ff3ba43e868cd2
Jan Henrik Lenes <jhlenes@outlook.com>
2017-01-23 21:37:34 +0100

    Now connects to available port

--- connection.py
+++ connection.py
@@ -1,10 +1,41 @@
 import time
 import serial
 import urllib.request
+import sys
+import glob
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 # Open connection
-ser = serial.Serial('/dev/ttyACM0', 9600)
-#ser = serial.Serial('COM4', 9600)
+ports = serial_ports()
+print(ports[0])
+ser = serial.Serial(ports[0], 9600, timeout=5)
 
 while True:
     
